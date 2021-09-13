957061ce7280417512d15ba87f548484007255a2
rosstimo <rosstimo@isu.edu>
2021-09-13 08:55:16 -0600

    function with scope and lifetime example

--- MethodExamples/MethodExamples.vb
+++ MethodExamples/MethodExamples.vb
@@ -1,26 +1,32 @@
 ﻿Module MethodExamples
 
+    Dim junk As Boolean
+
     Sub Main()
-        Dim robGaveMeMoney As Boolean
+        Dim firstNumber As Integer = 7
+        Dim secondNumber As Integer = 6
 
-        'robGaveMeMoney = True
-
-        If robGaveMeMoney Then
-            RobIsCool()
-        Else
-            RobIsNotCool()
-        End If
+        Sum(firstNumber, secondNumber)
 
         Console.Read()
     End Sub
 
-    Sub RobIsCool()
-        Console.WriteLine("Hi Rob!!")
-    End Sub
 
-    Sub RobIsNotCool()
-        Console.WriteLine("Rob is not here, let's make fun of him...")
-    End Sub
+
+
+    Function Sum(ByVal numOne As Integer, ByVal numTwo As Integer) As Integer
+        Console.WriteLine($"in sum secondNumber is {numTwo}")
+        Return numOne + numTwo
+    End Function
+
+    Function RandomNumber() As Integer
+        Dim _randomNumber As Integer
+
+        'TODO figure out how to get a random number...
+
+        Return _RandomNumber
+    End Function
+
 
 
 End Module
