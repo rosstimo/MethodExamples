1ddc276283bb4216fcdbf815fd9c4372b49742ef
rosstimo <rosstimo@isu.edu>
2021-09-15 08:57:59 -0600

    add random number, case statement, padleft, strdup examples

--- MethodExamples/MethodExamples.vb
+++ MethodExamples/MethodExamples.vb
@@ -3,11 +3,33 @@
     Dim junk As Boolean
 
     Sub Main()
-        Dim firstNumber As Integer = 7
-        Dim secondNumber As Integer = 6
+        'Dim firstNumber As Integer = 7
+        'Dim secondNumber As Integer = 6
 
-        Sum(firstNumber, secondNumber)
+        'Sum(firstNumber, secondNumber)
 
+        For column = 1 To 5
+            For row = 1 To 8
+
+                RandomNumber()
+
+                'Select Case RandomNumber()
+                '    Case <= 5
+                '        Console.WriteLine("You are too young to ride.")
+                '    Case 6 To 10
+                '        Console.WriteLine("You can only ride with an adult")
+                '    Case 11 To 35
+                '        Console.WriteLine("Enjoy the ride!")
+                '    Case >= 36
+                '        Console.WriteLine("Don't forget your heart pills!!")
+                '    Case Else
+                '        Console.WriteLine("Things Got Weird!!")
+                'End Select
+
+            Next
+            Console.WriteLine()
+            'Console.WriteLine(StrDup(80, "-"))
+        Next
         Console.Read()
     End Sub
 
@@ -21,8 +43,16 @@
 
     Function RandomNumber() As Integer
         Dim _randomNumber As Integer
-
+        Dim junk As String
         'TODO figure out how to get a random number...
+
+        _randomNumber = CInt(Rnd() * 1000)
+
+
+        junk = CStr(_randomNumber).PadLeft(4) & " |"
+
+        Console.Write(junk)
+
 
         Return _RandomNumber
     End Function
